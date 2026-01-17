563183f083a29e7c39c689553309549b949d2429
zxl hhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2026-01-17 22:02:43 +0800

    Fix WebUI link styling and attributes.
    Adapt luci-theme-openwrt-2020 theme.

--- htdocs/luci-static/resources/view/smartdns/smartdns.js
+++ htdocs/luci-static/resources/view/smartdns/smartdns.js
@@ -71,7 +71,7 @@ function smartdnsRenderStatus(res) {
 			var protocol = window.location.protocol;
 			var hostname = window.location.hostname;
 			var uiLink = protocol + "//" + hostname + ":" + uiPort;
-			renderHTML +=  '&#160; <a class="cbi-button cbi-button-positive" ' +  'href="' + uiLink + '" target="_blank">' +  _("Open the WebUI") +  '</a>';
+			renderHTML +=  '&#160; <a class="btn cbi-button cbi-button-positive" ' +  'href="' + uiLink + '" target="_blank">' +  _("Open the WebUI") +  '</a>';
 		}
 	} else {
 		renderHTML += "<span style=\"color:red;font-weight:bold\">SmartDNS - " + _("NOT RUNNING") + "</span>";
